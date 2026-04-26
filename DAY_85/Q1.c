3801796b62dc104bee24ccccd14952a14c6ff1b4
aastha chaudhary <aasthach945@gmail.com>
2026-04-26 22:18:23 +0530

    Create Q1.c

---
+++ DAY_85/Q1.c
@@ -0,0 +1,18 @@
+int findMin(int* nums, int numsSize) {
+    int left = 0, right = numsSize - 1;
+
+    while (left < right) {
+        int mid = left + (right - left) / 2;
+
+        // If mid element is greater than right, min is in right half
+        if (nums[mid] > nums[right]) {
+            left = mid + 1;
+        } 
+        // Otherwise, min is in left half (including mid)
+        else {
+            right = mid;
+        }
+    }
+
+    return nums[left];
+}
